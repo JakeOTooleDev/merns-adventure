324f9b3ce3d131e3bd0c8ca8cef3e70c135a4c0c
Jake O'Toole <jakeotooledev@gmail.com>
2021-05-25 17:22:48 -0500

    Created UserDetail Component and replaced UserDetail in App with import of component.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import * as Realm from "realm-web";
 
+import UserDetail from "./components/UserDetail";
+
 // * Created Login and UserDetail following the Realm Web quickstart guide: https://docs.mongodb.com/realm/web/react-web-quickstart/
 const REALM_APP_ID = "mernadventure-ydamf";
 const app = new Realm.App({ id: REALM_APP_ID });
 
-const UserDetail = ({ user }) => (
-  <div>
-    <h1>Loggid in with anonymus id: {user.id}</h1>
-  </div>
-);
+// const UserDetail = ({ user }) => (
+//   <div>
+//     <h1>Loggid in with anonymus id: {user.id}</h1>
+//   </div>
+// );
 
 const Login = ({ setUser }) => {
   const loginAnonymous = async () => {
@@ -31,13 +33,19 @@ const player = mongodb.db("mernAdventure").collection("player");
 function App() {
   const [user, setUser] = useState(app.currentUser);
 
-  console.log(player);
+  const getPlayer = async () => {
+    const players = await player.find({});
+    console.log(players);
+  };
 
   return (
     <div className="App">
       <h1>Hello World</h1>
       <div>
         {user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+      </div>
+      <div>
+        <button onClick={() => getPlayer()}>Find players</button>
       </div>
     </div>
   );
---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,8 @@
+// * Created Login and UserDetail following the Realm Web quickstart guide: https://docs.mongodb.com/realm/web/react-web-quickstart/
+export const UserDetail = ({ user }) => (
+  <div>
+    <h1>Loggid in with anonymus id: {user.id}</h1>
+  </div>
+);
+
+export default UserDetail;
