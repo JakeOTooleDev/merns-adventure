2009fbebb1f213307cf831dee92ab82414c6262a
Jake O'Toole <jakeotooledev@gmail.com>
2021-05-25 19:43:44 -0500

    RealmContext now provides the app and the module Realm.

--- src/hooks/context.jsx
+++ src/hooks/context.jsx
@@ -1,9 +1,14 @@
 import { createContext, useContext } from "react";
+import * as Realm from "realm-web";
 
-export const RealmContext = createContext({ test: "this is a test" });
+// * Created Login and UserDetail following the Realm Web quickstart guide: https://docs.mongodb.com/realm/web/react-web-quickstart/
+const REALM_APP_ID = "mernadventure-ydamf";
+const app = new Realm.App({ id: REALM_APP_ID });
+
+export const RealmContext = createContext({});
 
 export const RealmProvider = ({ children }) => (
-  <RealmContext.Provider value={{ test: "this is a test" }}>
+  <RealmContext.Provider value={{ app, Realm }}>
     {children}
   </RealmContext.Provider>
 );
