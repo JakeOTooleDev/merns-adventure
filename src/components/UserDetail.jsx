3794c8a8d5e68aef34542ba6da759894b57f7780
Jake O'Toole <jakeotooledev@gmail.com>
2021-05-25 19:44:17 -0500

    Removed console log and removed imports that will not be needed.

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,9 +1,5 @@
-import { useRealm } from "../hooks/context";
-
 // * Created Login and UserDetail following the Realm Web quickstart guide: https://docs.mongodb.com/realm/web/react-web-quickstart/
 export const UserDetail = ({ user }) => {
-  const app = useRealm();
-  console.log(app);
   return (
     <div>
       <h1>Loggid in with anonymus id: {user.id}</h1>
