12bd44bcfa9889772cdf0e34f93255a42436fbf5
Jake O'Toole <jakeotooledev@gmail.com>
2021-05-28 16:50:20 -0500

    Added DBProvider. This will be used to access the database throughout the app.

--- src/hooks/context.jsx
+++ src/hooks/context.jsx
@@ -6,6 +6,7 @@ const REALM_APP_ID = "mernadventure-ydamf";
 const app = new Realm.App({ id: REALM_APP_ID });
 
 export const RealmContext = createContext({});
+export const DBContext = createContext({});
 
 export const RealmProvider = ({ children }) => (
   <RealmContext.Provider value={{ app, Realm }}>
@@ -13,4 +14,9 @@ export const RealmProvider = ({ children }) => (
   </RealmContext.Provider>
 );
 
+export const DBProvider = ({ children }) => (
+  <DBContext.Provider value={{ test: "hello" }}>{children}</DBContext.Provider>
+);
+
 export const useRealm = () => useContext(RealmContext);
+export const useDB = () => useContext(DBContext);
