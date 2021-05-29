741e592a3d3519e508d142e78d442395de07105c
Jake O'Toole <jakeotooledev@gmail.com>
2021-05-29 16:13:32 -0500

    Starting layout of view for when you are playing the game.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,8 +3,18 @@ import UserDetails from "../components/UserDetail";
 export const Main = ({ currentPlayer, currentUser }) => {
   return (
     <>
-      <h1>Hello World</h1>
-      <UserDetails currentUser={currentUser} currentPlayer={currentPlayer} />
+      <header>
+        <h1>MERN's Point and Click Adventure</h1>
+        <UserDetails currentUser={currentUser} currentPlayer={currentPlayer} />
+      </header>
+      <section aria-label="gameplay">The scene will be located here.</section>
+      <section aria-label="communication">
+        Communications to the player will appear here.
+      </section>
+      <section aria-label="inventory">
+        The user's inventory will be shown here
+      </section>
+      <nav>Any menu options or navigation will go here.</nav>
     </>
   );
 };
