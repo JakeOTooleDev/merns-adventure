0099027e18e12ccd515d6daf075435f2a5e276c3
Jake O'Toole <jakeotooledev@gmail.com>
2021-06-05 14:56:33 -0500

    Added svg for living room.

--- src/components/scences/living-room/LivingRoom.jsx
+++ src/components/scences/living-room/LivingRoom.jsx
@@ -1,3 +1,7 @@
-export const LivingRoom = () => <div>Living Room</div>;
+import { ReactComponent as LivingRoomLinesOnly } from "./livingRoom_linesOnly.svg";
+
+export const LivingRoom = ({ className }) => (
+  <LivingRoomLinesOnly className={className} />
+);
 
 export default LivingRoom;
