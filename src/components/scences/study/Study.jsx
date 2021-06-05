9bb06b823d8f6ba93b94b5b0a7169fb988c2860e
Jake O'Toole <jakeotooledev@gmail.com>
2021-06-05 14:38:46 -0500

    Added study svg.

--- src/components/scences/study/Study.jsx
+++ src/components/scences/study/Study.jsx
@@ -1,3 +1,7 @@
-export const Study = () => <div>Study</div>;
+import { ReactComponent as StudyLinesOnly } from "./study_linesOnly.svg";
+
+export const Study = ({ className }) => (
+  <StudyLinesOnly className={className} />
+);
 
 export default Study;
