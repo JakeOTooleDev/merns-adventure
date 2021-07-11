1d83685578e6ec1ed583bc79884592850726e922
Jake O'Toole <jakeotooledev@gmail.com>
2021-07-11 15:32:08 -0500

    Added functionality for logging in with an email address and password. Added ability to logout.

--- src/App.js
+++ src/App.js
@@ -18,47 +18,49 @@ function App() {
   });
   const [currentUser, setCurrentUser] = useState({});
   const [players, setPlayers] = useState({});
-  // * https://reactjs.org/docs/hooks-reference.html#useref
-  // ? Still unsure if useRef is the proper hook to use in this case.
-  // ? I think creating the mongodb object would make more sense if a normal login/logout setup existed. You'd want to re-render/update mongodb if a new user logged in.
-  // let mongodb = useRef({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    const REALM_APP_ID = "mernadventure-ydamf";
-    const app = new Realm.App({ id: REALM_APP_ID });
+  const REALM_APP_ID = "mernadventure-ydamf";
+  const app = new Realm.App({ id: REALM_APP_ID });
 
-    const loginTestUser = async () => {
-      // * MongoDB Realm email/password login: https://docs.mongodb.com/realm/web/authenticate/#std-label-web-login-email-password
-      const testCredentials = Realm.Credentials.emailPassword(
-        "test@test.com",
-        "testtest"
-      );
-      try {
-        const user = await app.logIn(testCredentials);
-        setCurrentUser(user);
-      } catch (err) {
-        console.error(err);
-      }
+  const logOutUser = async () => {
+    try {
+      console.log(app.currentUser);
+      await app.currentUser.logOut();
+      console.log(app.currentUser);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-      // * Created collection handle following the MongoDB Data Access setup guide: https://docs.mongodb.com/realm/web/mongodb/#set-up-your-project
-      const mongodb = app.currentUser.mongoClient("mongodb-atlas");
-      const playerCollection = mongodb
-        .db("mernsAdventure")
-        .collection("players");
-      setPlayers(playerCollection);
-      console.log("currentUser", app.currentUser);
-      try {
-        // Have to use playerCollection, because players will not have a value until after the first render cycle
-        const player = await playerCollection.findOne({
-          _id: app.currentUser.id,
-        });
-        setCurrentPlayer(player);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    loginTestUser();
-  }, []);
+  const loginTestUser = async (event) => {
+    event.preventDefault();
+    // * MongoDB Realm email/password login: https://docs.mongodb.com/realm/web/authenticate/#std-label-web-login-email-password
+    const credentials = Realm.Credentials.emailPassword(email, password);
+    try {
+      const user = await app.logIn(credentials);
+      setCurrentUser(user);
+    } catch (err) {
+      console.error(err);
+    }
+
+    // * Created collection handle following the MongoDB Data Access setup guide: https://docs.mongodb.com/realm/web/mongodb/#set-up-your-project
+    const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+    const playerCollection = mongodb.db("mernsAdventure").collection("players");
+    setPlayers(playerCollection);
+
+    try {
+      // Have to use playerCollection, because players will not have a value until after the first render cycle
+      const player = await playerCollection.findOne({
+        _id: app.currentUser.id,
+      });
+      setCurrentPlayer(player);
+    } catch (err) {
+      console.error(err);
+    }
+    console.log("User Logged In");
+  };
 
   const updatePlayer = async () => {
     try {
@@ -74,13 +76,34 @@ function App() {
   return (
     <div className={`${styles.outer}`}>
       <div>
-        <form>
+        <form onSubmit={loginTestUser}>
           <label htmlFor="email">E-mail</label>
-          <InputText id="email" />
+          <InputText
+            id="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+          />
           <label htmlFor="password">Password</label>
-          <Password id="password" />
+          <Password
+            id="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          />
           <Button label="Submit" icon="pi pi-check" />
         </form>
+      </div>
+      <div>
+        <Button
+          label="Logout"
+          onClick={() => {
+            console.log("Get me out of here!");
+            logOutUser();
+          }}
+        />
       </div>
       {/* <Main
         className={styles.inner}
