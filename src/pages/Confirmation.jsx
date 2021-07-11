5b025e5f5e115f08b8c60bb8bb3b40959ce6b7d4
Jake O'Toole <jakeotooledev@gmail.com>
2021-07-11 17:26:30 -0500

    Added redirect to home page when email address is confirmed.

--- src/pages/Confirmation.jsx
+++ src/pages/Confirmation.jsx
@@ -1,4 +1,8 @@
+import { useHistory } from "react-router-dom";
+
 export const Confirmation = ({ app }) => {
+  // https://reactrouter.com/web/api/Hooks/usehistory
+  let history = useHistory();
   // MongoDB Confirmation instructions on https://docs.mongodb.com/realm/authentication/email-password/#std-label-email-password-authentication-confirmation
   // To get query I followed instructions on https://www.sitepoint.com/get-url-parameters-with-javascript/
   // Later found that react-router-dom has a hook that does something very similiar https://reactrouter.com/web/example/url-params
@@ -10,7 +14,8 @@ export const Confirmation = ({ app }) => {
   console.log(token, tokenId);
   const confirmMernUser = async () => {
     try {
-      app.emailPasswordAuth.confirmUser(token, tokenId);
+      await app.emailPasswordAuth.confirmUser(token, tokenId);
+      history.push("/");
     } catch (err) {
       console.error(err);
     }
