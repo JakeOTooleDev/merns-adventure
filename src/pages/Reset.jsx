e1fdee1ca9348a9e421ab21a2c6e6d2b04840c01
Jake O'Toole <jakeotooledev@gmail.com>
2021-07-12 18:42:27 -0500

    Restyled Confirmation and Reset pages to match the Authentication page.

--- src/pages/Reset.jsx
+++ src/pages/Reset.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import cx from "classnames";
 
 import { Button } from "primereact/button";
+import { Card } from "primereact/card";
 import { Password } from "primereact/password";
+
+import { Header } from "../components/layout/Header";
+
+import styles from "./Reset.module.scss";
 
 export const Reset = ({ app }) => {
   const [newPassword, setNewPassword] = useState("");
@@ -12,11 +18,11 @@ export const Reset = ({ app }) => {
   // To get query I followed instructions on https://www.sitepoint.com/get-url-parameters-with-javascript/
   // Later found that react-router-dom has a hook that does something very similiar https://reactrouter.com/web/example/url-params
   const queryString = window.location.search;
-  console.log(queryString);
+
   const urlParams = new URLSearchParams(queryString);
   const token = urlParams.get("token");
   const tokenId = urlParams.get("tokenId");
-  console.log(token, tokenId);
+
   const resetMernPassword = async (event) => {
     event.preventDefault();
     try {
@@ -28,18 +34,25 @@ export const Reset = ({ app }) => {
   };
   return (
     <div>
-      <h1>Reset Your Password!</h1>
-      <form onSubmit={resetMernPassword}>
-        <label htmlFor="newPassword">New Password</label>
-        <Password
-          id="newPassword"
-          onChange={(e) => {
-            setNewPassword(e.target.value);
-          }}
-          value={newPassword}
-        />
-        <Button label="Reset Password" icon="pi pi-check" />
-      </form>
+      <Header />
+      <div className="p-d-flex p-jc-center p-ai-center">
+        <Card className={cx("p-m-5", styles.reset)} title="Reset Your Password">
+          <form onSubmit={resetMernPassword}>
+            <label className="p-m-2" htmlFor="newPassword">
+              New Password
+            </label>
+            <Password
+              id="newPassword"
+              className="p-m-2"
+              onChange={(e) => {
+                setNewPassword(e.target.value);
+              }}
+              value={newPassword}
+            />
+            <Button className="p-m-2" label="Reset Password" icon="pi pi-check" />
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
